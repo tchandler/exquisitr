0687dd33a01c80bbf4363677d8006fdd5e082470
Richard Howard <richard@howard.io>
2018-04-20 17:02:45 -0400

    Team 3

--- src/components/team.js
+++ src/components/team.js
@@ -19,7 +19,7 @@ import team7Props from '../props/team7'
 import team8Props from '../props/team8'
 
 const DEV = true
-const MY_TEAM = 1
+const MY_TEAM = 3
 
 const componentsMap = {
   Team1, Team2, Team3, Team4, Team5,
