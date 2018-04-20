1eb435062ecb379d19c8566bfd34f7b6e4749d2f
Ryan Hinkel <ryanhinkel@gmail.com>
2018-04-20 17:09:32 -0400

    Tweak styles

--- src/components/team8.js
+++ src/components/team8.js
@@ -15,14 +15,16 @@ export default class Team1 extends Component {
           <img className='contact-img' src='https://media0.giphy.com/media/R4hiYuzvIgEKs/giphy-downsized.gif' />
         </div>
         <div className='sitemap'>
+          <h2>Site Map</h2>
           <ul>
             <li><a>Hey, Wait, Where Am I?</a></li>
             <li><a>It's so dark and cold</a></li>
             <li><a>Can anyone hear me?</a></li>
-            <li><a>Privacy Policy</a></li>
+            <li><a className='privacy'>Privacy Policy</a></li>
           </ul>
         </div>
         <div className='copyright'>
+          <h2>Legal Policy</h2>
           <img src='https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.clipartbest.com%2Fcliparts%2FRTA%2F7E6%2FRTA7E6Ekc.png&f=1' />
         </div>
       </div>
